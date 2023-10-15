924dc470b53ab6b723225bee784d004682ef7ba7
= <raphael.vasseur26@proton.me>
2023-10-15 12:15:09 +0200

    the overall set of colors has been changed and the design has evolved

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,22 @@ function App() {
   return (
     <>
       <div className='side-nav'>
-        <a href="">
+        <a href="https://github.com/p0ubelle/empyrean">
           <img src="src\assets\img\empyrean_logo.png" className="logo empyrean" alt="Empyrean Logo" />
         </a>
         
-        <button className="button-side-nav" href="buttonbutton">test1</button>
-        <button className="button-side-nav" href="buttonbutton">test2</button>
-        <button className="button-side-nav" href="buttonbutton">test3</button>
-        <button className="button-side-nav" href="buttonbutton">test4</button>
+        <button className="side-nav-button" href="buttonbutton">tier list</button>
+        <button className="side-nav-button" href="buttonbutton">test2</button>
+        <button className="side-nav-button" href="buttonbutton">test3</button>
+        <button className="side-nav-button" href="buttonbutton">test4</button>
       </div>
       
 
-      <h1>Vite + React + Tauri</h1>
+      <h1>Empyrean</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          test <code>&lt;code&gt;</code> HTML
-        </p>
       </div>
       <p className="read-the-docs">
         by poubelle26, first deployement [15/10/23 11:09]
